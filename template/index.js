736d4a1f954dfebe3c97dbfc7523179f3fd4f609
杨俊宁 <youngjuning@aliyun.com>
2019-12-14 19:32:58 +0800

    开启mobx严格模式

--- template/index.js
+++ template/index.js
@@ -3,7 +3,11 @@
  */
 
 import { AppRegistry } from 'react-native'
+import { configure } from 'mobx'
 import App from './App'
 import { name as appName } from './app.json'
+
+// 设置 mobx 不允许在动作外部修改状态
+configure({ enforceActions: 'observed' })
 
 AppRegistry.registerComponent(appName, () => App)
