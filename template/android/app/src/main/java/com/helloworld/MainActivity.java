c8bf39cfb8ce22e885b0d33113a50732ae01a199
杨俊宁 <youngjuning@aliyun.com>
2019-12-14 19:18:40 +0800

    Android 文字不随系统变化

--- template/android/app/src/main/java/com/helloworld/MainActivity.java
+++ template/android/app/src/main/java/com/helloworld/MainActivity.java
@@ -2,6 +2,9 @@ package com.helloworld;
 
 import com.facebook.react.ReactActivity;
 
+import android.content.res.Configuration;
+import android.content.res.Resources;
+
 public class MainActivity extends ReactActivity {
 
   /**
@@ -11,5 +14,23 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "HelloWorld";
+  }
+
+  // 让文字不随系统文字变化：http://t.cn/Rs26Veb
+  @Override
+  public void onConfigurationChanged(Configuration newConfig) {
+    if (newConfig.fontScale != 1)//非默认值
+    getResources();
+    super.onConfigurationChanged(newConfig);
+  }
+  @Override
+  public Resources getResources() {
+    Resources res = super.getResources();
+    if (res.getConfiguration().fontScale != 1) {//非默认值
+      Configuration newConfig = new Configuration();
+      newConfig.setToDefaults();//设置默认
+      res.updateConfiguration(newConfig, res.getDisplayMetrics());
+    }
+    return res;
   }
 }
