43c495aabde203e407f78575c45cefdf2abb93e9
杨俊宁 <youngjuning@aliyun.com>
2019-12-16 11:45:41 +0800

    设置字体大小不随系统字体大小变化而变化

--- template/android/app/src/main/java/com/helloworld/MainActivity.java
+++ template/android/app/src/main/java/com/helloworld/MainActivity.java
@@ -15,22 +15,4 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "HelloWorld";
   }
-
-  // 让文字不随系统文字变化：http://t.cn/Rs26Veb
-  @Override
-  public void onConfigurationChanged(Configuration newConfig) {
-    if (newConfig.fontScale != 1)//非默认值
-    getResources();
-    super.onConfigurationChanged(newConfig);
-  }
-  @Override
-  public Resources getResources() {
-    Resources res = super.getResources();
-    if (res.getConfiguration().fontScale != 1) {//非默认值
-      Configuration newConfig = new Configuration();
-      newConfig.setToDefaults();//设置默认
-      res.updateConfiguration(newConfig, res.getDisplayMetrics());
-    }
-    return res;
-  }
 }
--- template/index.js
+++ template/index.js
@@ -2,11 +2,14 @@
  * @format
  */
 
-import { AppRegistry } from 'react-native'
+import { AppRegistry, Text, TextInput } from 'react-native'
 import { configure } from 'mobx'
 import App from './App'
 import { name as appName } from './app.json'
 
+// 设置字体大小不随系统字体大小变化而变化
+Text.defaultProps = { ...Text.defaultProps, allowFontScaling: false }
+TextInput.defaultProps = { ...TextInput.defaultProps, allowFontScaling: false }
 // 设置 mobx 不允许在动作外部修改状态
 configure({ enforceActions: 'observed' })
 
