a914ec85e53eef3e4cf5a97386d5ce24a2ca3113
杨俊宁 <youngjuning@aliyun.com>
2020-01-19 11:44:48 +0800

    更新 react-navigation 到 5

--- template/index.js
+++ template/index.js
@@ -4,6 +4,7 @@
 
 import { AppRegistry, Text, TextInput } from 'react-native'
 import { configure } from 'mobx'
+import 'react-native-gesture-handler'
 import App from './App'
 import { name as appName } from './app.json'
 
